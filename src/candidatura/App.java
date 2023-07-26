18af2fc1db3f95571d64b95a8d065b0df6c4cc14
00Theory <marco200002010@outlook.com>
2023-07-25 23:05:32 -0300

    Case 1

---
+++ src/candidatura/App.java
@@ -0,0 +1,21 @@
+package candidatura;
+public class App {
+    public static void main(String[] args) throws Exception {
+        analisarSalarioPretendido(2500.00);
+        analisarSalarioPretendido(2000.00);
+        analisarSalarioPretendido(1950.00);
+    }
+
+    static void analisarSalarioPretendido(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }
+        else if (salarioBase == salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO, COM CONTRA PROPOSTA");
+        }
+        else{
+            System.out.println("AGUARDANDO RESULTADO DOS PRÓXIMOS CANDIDATOS");
+        }
+    }
+}
