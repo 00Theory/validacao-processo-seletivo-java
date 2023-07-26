386dc4b7f1753be0cf64ccf45bab20d3c70ff80d
00Theory <marco200002010@outlook.com>
2023-07-25 23:27:06 -0300

    Denominações das listas

--- src/candidatura/Case3.java
+++ src/candidatura/Case3.java
@@ -2,12 +2,18 @@ package candidatura;
 
 public class Case3 {
     public static void main(String[] args) {
+        MandarParaRH();
+    }
+
+    static void MandarParaRH(){
         String [] candidatosEscolhidos = {"FELIPE","MÁRCIA","FABRÍCIO","MIRELA","DANIELA"};
 
+        System.out.println("Lista com índice numérico: ");
         for (int i = 0; i < candidatosEscolhidos.length; i++){
         System.out.println("O candidato nº " + (i+1) + " é o(a) " + candidatosEscolhidos[i]);
         }
 
+        System.out.println("Lista sem índice:");
         for (String candidato:candidatosEscolhidos){
             System.out.println(candidato);
         }
